02b3add7a7f857f37b587dd91ada57dd3fc6258b
niallhewlett <niallhewlett123@gmail.com>
2021-10-18 12:47:34 +0100

    POOLER-8 updated password rules

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -13,6 +13,13 @@ trait PasswordValidationRules
      */
     protected function passwordRules()
     {
-        return ['required', 'string', new Password, 'confirmed'];
+        return ['required', 
+        'string',
+        'min:8', //must have at least 8 chars
+        'regex:/[a-z]/',  //must contain at least one lowercase char
+        'regex:/[A-Z]/',  //must contain at least one uppercase char
+        'regex:/[0-9]/',  //must contain at least one number
+        'regex:/[@$!%*#?&]/', //must contain at least one special char, 
+        ];
     }
 }
