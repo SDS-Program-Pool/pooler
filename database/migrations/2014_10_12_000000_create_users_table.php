063663c1cc5be872df755926b8e548bf9c357715
Christopher Taylor <me@christophertaylor.me.uk>
2021-10-21 22:34:59 +0100

    POOLER-19 DB hotfix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->UnsignedInteger('id')->unique();
+            $table->primary('id');
             $table->string('username');
             $table->string('first_name');
             $table->string('last_name');
