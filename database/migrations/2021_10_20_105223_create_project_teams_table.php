bee17a6c723d4f0268b5d585e0c8d904d17b2443
Christopher Taylor <me@christophertaylor.me.uk>
2021-10-27 15:54:22 +0100

    POOLER-24 - DB Structure for Project Teams

--- database/migrations/2021_10_20_105223_create_project_teams_table.php
+++ database/migrations/2021_10_20_105223_create_project_teams_table.php
@@ -14,8 +14,16 @@ class CreateProjectTeamsTable extends Migration
     public function up()
     {
         Schema::create('project_teams', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
+            $table->primary('id');
+            $table->uuid('project_id');
+            $table->BigInteger('user_id')->unsigned()->nullable(); // Project Member
+            $table->boolean('is_owner');
             $table->timestamps();
+            $table->foreign('project_id')->references('id')->on('projects')
+            ->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('set null');
         });
     }
 
