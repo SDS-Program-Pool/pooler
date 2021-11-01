9c2446546bdf42316e96f0a72a0bbcb78320fe07
Christopher Taylor <me@christophertaylor.me.uk>
2021-11-01 11:02:57 +0000

    POOLER-36 - Indiv Team creation updates.

--- app/Actions/Team/IndividualTeamCreation.php
+++ app/Actions/Team/IndividualTeamCreation.php
@@ -11,17 +11,26 @@ class IndividualTeamCreation
 {
     public function create(Request $request, $project)
     {
+        // create a team
+        // add team members
+        // could have a seperate file class for team member creation
 
         $validated = $request->validate([
             //'team_type' => 'required|in:IndividualTeamCreation,FeatureBranchTeamCreation,TeamCreation',
             //'file' => 'required|mimes:tar,zip',
         ]);
 
+        // Uncomment this at a later date once main dev stuff is sorted ^^^^
+
         $team = new Team;
 
         $team->user_id = Auth::id();
+        $team->project_id = $project->id;
+        $team->isOwner = TRUE;
 
-        dd($project->id);
+        $team->save();
+
+        return ($team);
 
     }
 
