02b61e5bd5c6849214cf4ee54db79d2f17d6e7fa
Christopher Taylor <me@christophertaylor.me.uk>
2021-11-09 23:27:46 +0000

    POOLER-61 DB Updates

--- app/Http/Controllers/Student/ProjectMarkController.php
+++ app/Http/Controllers/Student/ProjectMarkController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ProjectMarkAllocation;
+
 
 class ProjectMarkController extends Controller
 {
     public function index()
     {
 
-        $mark_data = Project::whereUserId(Auth::id())->get();
+        $mark_data = ProjectMarkAllocation::whereUserId(Auth::id())->get();
 
         return view('mark.index', compact('mark_data'));
     }
--- database/migrations/2021_11_09_231102_create_project_mark_allocations_table.php
+++ database/migrations/2021_11_09_231102_create_project_mark_allocations_table.php
@@ -14,8 +14,16 @@ class CreateProjectMarkAllocationsTable extends Migration
     public function up()
     {
         Schema::create('project_mark_allocations', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
+            $table->primary('id');
+            $table->uuid('project_id');
+            $table->BigInteger('user_id')->unsigned()->nullable(); // Project Member
+            $table->boolean('taken_by_user')->nullable();
             $table->timestamps();
+            $table->foreign('project_id')->references('id')->on('projects')
+            ->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('set null');
         });
     }
 
