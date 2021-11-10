1ce5016f0f41e26b690a184b7abe27c5556d06f5
Christopher Taylor <me@christophertaylor.me.uk>
2021-11-10 16:17:44 +0000

    POOLER-63 Move
    I genuinely can't think. Moved into seperate file and namespace. COme back to this later.

--- app/Http/Controllers/Student/ProjectController.php
+++ app/Http/Controllers/Student/ProjectController.php
@@ -11,6 +11,7 @@ use App\Actions\Team\FeatureBranchTeamCreation;
 use App\Actions\Team\TeamCreation;
 use App\Actions\Project\ProjectCreate;
 use App\Actions\CodeUpload\Upload;
+use App\Actions\CodeAllocation\Allocation;
 
 use App\Models\Project;
 use App\Models\ProjectMarkAllocation;
@@ -22,53 +23,10 @@ class ProjectController extends Controller
     public function index()
     {
 
+        $test = new Allocation;
+        $test = $test->first_allocation();
 
-        $project_data = Project::get('id');
-        $project_allocation_data = ProjectMarkAllocation::all('project_id');
-    
-       foreach($project_data as $data)
-       {
-           $project_id_array[] = ($data->id);
-       }
-
-       foreach($project_allocation_data as $data)
-       {
-           dump($data);
-           $project_alloc_id_array[] = ($data->project_id);
-       }
-
-       $diff = array_diff($project_id_array, $project_alloc_id_array); // Projects that have NOT been allocated i.e not in our ProjectAlloc table
-
-       // Time to Allocate
-    
-       // find the users who are in team_members who are on this proj.
-       // Search users table to generate array
-       // Remove those working on it from the array
-       // random allocate provided that they have not got more than 3 (weighting system.)
-
-
-       // We know what projects have not been allocated
-       // We need to run a sql query and get all data e.g user_id
-
-        $all_users = User::get('id');
-
-        $test = Project::with('team_members')->get();
-
-        dump($test);
-    
-        
-       
-
-
-
-
-
-        
-
-
-
-
-
+        dd($test);
 
         //$project_data = Project::whereUserId(Auth::id())->get();
 
