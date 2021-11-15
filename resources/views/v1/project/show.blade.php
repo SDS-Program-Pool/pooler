e2c8a2f713f5144efc52de323b2e0053d5a31987
Isaac <isaac_dolphin@outlook.com>
2021-11-15 13:01:22 +0000

    Update to front-end

--- resources/views/v1/project/show.blade.php
+++ resources/views/v1/project/show.blade.php
@@ -1,10 +1,15 @@
 @extends('v1.layouts.app')
 @section('title', 'View Project')
 
-
 @extends('v1.project.menu')
 
 @section('content')
+
+<!-- TO DO:
+
+  => Fix tabs
+
+-->
 
 <h1 class="govuk-heading-xl">Project - {{ $project_data->name }}</h1>
 
@@ -46,7 +51,55 @@
   </ul>
   <div class="govuk-tabs__panel" id="overview">
     <h2 class="govuk-heading-l">Overview</h2>
-    <table class="govuk-table">
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Project Name
+        </dt>
+        <dd class="govuk-summary-list__value">
+          {{ $project_data->name }}
+        </dd>
+        <dd class="govuk-summary-list__actions">
+          <a class="govuk-link" href="#">
+            Change<span class="govuk-visually-hidden"> project name</span>
+          </a>
+        </dd>
+      </div>
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Upload Date
+        </dt>
+        <dd class="govuk-summary-list__value">
+          {{ $project_data->created_at}}
+        </dd>
+        <dd class="govuk-summary-list__actions"></dd>
+      </div>
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          File
+        </dt>
+        <dd class="govuk-summary-list__value">
+          test.zip
+        </dd>
+        <dd class="govuk-summary-list__actions">
+          <a class="govuk-link" href="#">
+            Download<span class="govuk-visually-hidden"> file</span>
+          </a>
+        </dd>
+      </div>
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Status
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <strong class="govuk-tag govuk-tag--green">
+            Finished
+          </strong>
+        </dd>
+        <dd class="govuk-summary-list__actions"></dd>
+      </div>
+    </dl>
+    <!--<table class="govuk-table">
       <thead class="govuk-table__head">
         <tr class="govuk-table__row">
           <th scope="col" class="govuk-table__header">Case manager</th>
@@ -71,40 +124,63 @@
           <td class="govuk-table__cell">0</td>
         </tr>
       </tbody>
-    </table>
+    </table>-->
 
   </div>
-  <div class="govuk-tabs__panel govuk-tabs__panel--hidden" id="mark">
+  <!--  ADD BACK --hidden flag after __panel on each relavent div.  -->
+  <div class="govuk-tabs__panel govuk-tabs__panel" id="mark">
     <h2 class="govuk-heading-l">Mark</h2>
-    <table class="govuk-table">
-      <thead class="govuk-table__head">
-        <tr class="govuk-table__row">
-          <th scope="col" class="govuk-table__header">Case manager</th>
-          <th scope="col" class="govuk-table__header">Cases opened</th>
-          <th scope="col" class="govuk-table__header">Cases closed</th>
-        </tr>
-      </thead>
-      <tbody class="govuk-table__body">
-        <tr class="govuk-table__row">
-          <td class="govuk-table__cell">David Francis</td>
-          <td class="govuk-table__cell">24</td>
-          <td class="govuk-table__cell">18</td>
-        </tr>
-        <tr class="govuk-table__row">
-          <td class="govuk-table__cell">Paul Farmer</td>
-          <td class="govuk-table__cell">16</td>
-          <td class="govuk-table__cell">20</td>
-        </tr>
-        <tr class="govuk-table__row">
-          <td class="govuk-table__cell">Rita Patel</td>
-          <td class="govuk-table__cell">24</td>
-          <td class="govuk-table__cell">27</td>
-        </tr>
-      </tbody>
-    </table>
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Assessor 1
+        </dt>
+        <dd class="govuk-summary-list__value">
+          {{ $project_data->name }}
+        </dd>
+        <dd class="govuk-summary-list__actions">
+          <a class="govuk-link" href="#">
+            Change<span class="govuk-visually-hidden"> project name</span>
+          </a>
+        </dd>
+      </div>
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Assessor 2
+        </dt>
+        <dd class="govuk-summary-list__value">
+          {{ $project_data->created_at}}
+        </dd>
+        <dd class="govuk-summary-list__actions"></dd>
+      </div>
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Assessor 3
+        </dt>
+        <dd class="govuk-summary-list__value">
+          test.zip
+        </dd>
+        <dd class="govuk-summary-list__actions">
+          <a class="govuk-link" href="#">
+            Download<span class="govuk-visually-hidden"> file</span>
+          </a>
+        </dd>
+      </div>
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key">
+          Status
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <strong class="govuk-tag govuk-tag--green">
+            Finished
+          </strong>
+        </dd>
+        <dd class="govuk-summary-list__actions"></dd>
+      </div>
+    </dl>
 
   </div>
-  <div class="govuk-tabs__panel govuk-tabs__panel--hidden" id="mark-review">
+  <div class="govuk-tabs__panel govuk-tabs__panel" id="mark-review">
     <h2 class="govuk-heading-l">Mark Review</h2>
     <table class="govuk-table">
       <thead class="govuk-table__head">
