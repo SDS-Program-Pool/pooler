80c40370ce06ed4eccd7e51343bda4f211dcc368
Christopher Taylor <me@christophertaylor.me.uk>
2021-11-26 11:05:04 +0000

    Fortify CreateNewUser

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -31,6 +31,8 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
+            //'password' => $this->passwordRules(),
+            //'is_verified' => ['requied','boolean'],
             'password' => $this->passwordRules(),
             //'is_verified' => ['requied','boolean'],
         ])->validate();
