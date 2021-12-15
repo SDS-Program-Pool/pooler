06a574a75e32e42316394b518c28e97f1665b23b
Chris <ctaylor-c172@users.noreply.github.com>
2021-12-15 19:13:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,18 +15,12 @@ class DashboardController extends Controller
 
     public function index()
     {
-        
         $hour = date('H');
         $dayTerm = ($hour > 17) ? 'Evening' : (($hour > 12) ? 'Afternoon' : 'Morning');
 
-        return view('v1.dashboard.student',compact('dayTerm'));
-
-
-
-
+        return view('v1.dashboard.student', compact('dayTerm'));
 
         // for testing purposes only!!
-
 
         if (Auth::user()->hasRole('student')) {
             return view('v1.dashboard.student');
