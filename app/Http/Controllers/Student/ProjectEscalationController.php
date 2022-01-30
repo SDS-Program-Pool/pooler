b6a6d0ee2d06c398cf3126064987a54c6d7e3829
Christopher Taylor <me@christophertaylor.me.uk>
2022-01-30 16:16:46 +0000

    Updates

---
+++ app/Http/Controllers/Student/ProjectEscalationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Let a user escalate their project
+class ProjectEscalationController extends Controller
+{
+    public function store(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/Student/ProjectEscalationRequestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectEscalationRequestController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Allow a user to esalate their proejct to the upper marking team.
+    }
+}
