4ed826e0ae564e47e1e85cc64f7b8d5fb57b4bcf
Christopher Taylor <me@christophertaylor.me.uk>
2022-02-06 01:56:39 +0000

    Fix #80

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -14,11 +14,7 @@ trait PasswordValidationRules
         return [
             'required',
             'string',
-            'min:8', //must have at least 8 chars
-            'regex:/[a-z]/',  //must contain at least one lowercase char
-            'regex:/[A-Z]/',  //must contain at least one uppercase char
-            'regex:/[0-9]/',  //must contain at least one number
-            'regex:/[@$!%*#?&]/', //must contain at least one special char,
+            'min:8', 
         ];
     }
 }
